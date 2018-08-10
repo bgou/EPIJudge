3de303fc1e2439ee1432d2e333c4720057cd18de
Bruce Gou <brucegou@gmail.com>
2018-08-10 00:11:48 -0700

    StringIntegerInterconversion

--- epi_judge_java/epi/StringIntegerInterconversion.java
+++ epi_judge_java/epi/StringIntegerInterconversion.java
@@ -5,16 +5,28 @@ import epi.test_framework.TestFailure;
 public class StringIntegerInterconversion {
 
   public static String intToString(int x) {
-    // TODO - you fill in here.
-    return "";
+      boolean isNeg = x < 0;
+    String sign = (isNeg ? "-" : "");
+    StringBuilder sb = new StringBuilder();
+    do{
+        int l = x%10;
+      sb.append(Math.abs(l));
+      x /= 10;
+    } while (x != 0);
+
+    return sign + sb.reverse().toString();
   }
   public static int stringToInt(String s) {
-    // TODO - you fill in here.
-    return 0;
+    return (s.charAt(0) == '-' ? -1 : 1) *
+            s.substring(s.charAt(0) == '-' ? 1 : 0)
+            .chars().reduce(0, (result, c) -> {
+      return result * 10 + c-'0';
+    });
   }
   @EpiTest(testDataFile = "string_integer_interconversion.tsv")
   public static void wrapper(int x, String s) throws TestFailure {
-    if (!intToString(x).equals(s)) {
+      String res = intToString(x);
+      if (!res.equals(s)) {
       throw new TestFailure("Int to string conversion failed");
     }
     if (stringToInt(s) != x) {
