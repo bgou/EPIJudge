3c273d644cc257d58ae68b16493e379b61945b18
Bruce Gou <bruceg@qualtrics.com>
2018-08-24 22:56:32 -0700

    SearchFirstKey

--- epi_judge_java/epi/SearchFirstKey.java
+++ epi_judge_java/epi/SearchFirstKey.java
@@ -1,20 +1,33 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 import java.util.List;
+
 public class SearchFirstKey {
-  @EpiTest(testDataFile = "search_first_key.tsv")
+    @EpiTest(testDataFile = "search_first_key.tsv")
 
-  public static int searchFirstOfK(List<Integer> A, int k) {
-    // TODO - you fill in here.
-    return 0;
-  }
+    public static int searchFirstOfK(List<Integer> A, int k) {
+        int L = 0, R = A.size() - 1;
+        int result = -1;
+        while (L <= R) {
+            int M = L + (R - L) / 2;
+            if (A.get(M) < k) {
+                L = M + 1;
+            } else if (A.get(M) == k) {
+                result = M;
+                R = M - 1;
+            } else {
+                R = M - 1;
+            }
+        }
+        return result;
+    }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "SearchFirstKey.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    public static void main(String[] args) {
+        System.exit(GenericTest.runFromAnnotations(args,
+                                                   "SearchFirstKey.java",
+                                                   new Object() {}.getClass().getEnclosingClass()).ordinal());
+    }
 }
