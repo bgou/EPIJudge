5b28c25e874b27da92b208a5f0dcc6bedce3169f
Bruce Gou <bruceg@qualtrics.com>
2018-08-26 11:39:34 -0700

    IntSquareRoot

--- epi_judge_java/epi/IntSquareRoot.java
+++ epi_judge_java/epi/IntSquareRoot.java
@@ -5,8 +5,18 @@ public class IntSquareRoot {
   @EpiTest(testDataFile = "int_square_root.tsv")
 
   public static int squareRoot(int k) {
-    // TODO - you fill in here.
-    return 0;
+    long L=0, R = k;
+    while (L <= R) {
+      long M = L + (R-L)/2;
+      if (M*M >k) {
+        R = M-1;
+      }
+      else if (M*M <=k) {
+        L = M + 1;
+      }
+
+    }
+    return (int)L-1;
   }
 
   public static void main(String[] args) {
